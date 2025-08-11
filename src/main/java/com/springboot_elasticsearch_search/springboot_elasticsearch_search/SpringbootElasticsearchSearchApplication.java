1400330b19e0c9de2f947dc0b6a4f60352e46765
Vermamayank7176 <mayankverma10219@gmail.com>
2025-08-11 19:28:03 +0530

    Added docker-compose.yml

---
+++ src/main/java/com/springboot_elasticsearch_search/springboot_elasticsearch_search/SpringbootElasticsearchSearchApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_elasticsearch_search.springboot_elasticsearch_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticsearchSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootElasticsearchSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_elasticsearch_search/springboot_elasticsearch_search/SpringbootElasticsearchSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_elasticsearch_search.springboot_elasticsearch_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticsearchSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
